d465f685ae61c9648f068e0a6e72fd954845e239
lassefinderup <lassefinderup@users.noreply.github.com>
2017-06-01 15:55:47 +0200

    before array via constructur

---
+++ app/course.service.ts
@@ -0,0 +1,8 @@
+
+export class CourseService {
+    getCourses() : string[] {
+
+        // vi leger at det her ern et kald til en server
+        return ["kursus1", "kursus2", "kursus3"];
+    }
+}
--- app/courses.component.ts
+++ app/courses.component.ts
@@ -1,4 +1,30 @@
+//component bygget op fra bunden
+
+import {Component} from 'angular2/core'
+import {CourseService} from './course.service'
+
+@Component({
+    //css selector for host html element.
+    selector: 'courses',
+    //specifies the html that will be inserted in the DOM. Can be inline or separate file
+    // ngFor = #course er lokalvariablen, of er et keyword, courses objektet som loopes
+    template: `
+        <h2>Courses</h2>
+        {{ title }}
+        <ul>
+            <li *ngFor="#course of courses">
+                {{ course }}
+            </li>
+        </ul>
+        `
+})
 export class CoursesComponent {
-    
+    //decorator = attribute
+    title = "Her e r titlen!";
+    courses;
+
+    // constructor(courseService: CourseService){
+
+    // }
 
 }
